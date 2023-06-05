535d031ca0cd11cc0b0d363046c4d4ad2dc5f2bc
Kristina <kristina.ferm@gmail.com>
2023-06-05 21:01:13 +0200

    Some menu fixes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -43,91 +43,89 @@ export default function Nav() {
   };
 
   return (
-    <div className="papa-nav">
-      <div className="navigation">
-        <div className="hamburger" onClick={toggleHamburger}>
-          <Hamburger isOpen={hamburgerOpen} />
-        </div>
-
-        <h1 className="H1-Header">{headername}</h1>
-        
-        <Link to="/login">
-          <FaUserAlt id="User-icon" />
-        </Link>
-
-        <ul className={hamburgerOpen ? "show" : "hide"}>
-          <Link
-            to="/"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            Home
-          </Link>
-          <Link
-            to="/onduty"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            On Duty
-          </Link>
-          <Link
-            to="/about"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            About us
-          </Link>
-          <Link
-            to="/contact"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            Contact us
-          </Link>
-          <Link
-            to="/sos"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            SOS
-          </Link>{" "}
-          <Link
-            to="/posts"
-            className="nav-link"
-            onClick={() => setHamburgerOpen(false)}
-          >
-            Posts
-          </Link>
-        </ul>
-
-        <div className="Nav-fs">
-        <div className="logo-nav-div">
-        <img className="logo-nav" src={tiny} alt="Tiny-logo" />
-        </div>
-        
-          <Link to='/'>
-            <h2 className="h2-header">Home</h2>
-          </Link>
-          <Link to='/onduty'>
-            <h2 className="h2-header">OnDuty</h2>
-          </Link>
-          <Link to='/posts'>
-            <h2 className="h2-header">Posts</h2>
-          </Link>
-          <Link to='/about'>
-            <h2 className="h2-header">About</h2>
-          </Link>
-          <Link to='/contact'>
-            <h2 className="h2-header">Contact</h2>
-          </Link>
-          <Link to='/sos'>
-            <h2 className="h2-header">SOS</h2>
-          </Link>
-          <Link className="h2-header" to="/login">
-            <FaUserAlt id="User-icon-fs" />
-          </Link>
-        </div>
+    <div className="navigation">
+      <div className="hamburger" onClick={toggleHamburger}>
+        <Hamburger isOpen={hamburgerOpen} />
       </div>
+
+      <div className="logo-nav-div flex-item">
+        <img className="logo-nav" src={tiny} alt="Tiny-logo" />
+      </div>
+
+      <h1 className="H1-Header flex-item">{headername}</h1>
+
+      <Link to="/login">
+        <FaUserAlt id="User-icon" />
+      </Link>
+
+      <ul className={hamburgerOpen ? "show" : "hide"}>
+        <Link
+          to="/"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          Home
+        </Link>
+        <Link
+          to="/onduty"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          On Duty
+        </Link>
+        <Link
+          to="/about"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          About us
+        </Link>
+        <Link
+          to="/contact"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          Contact us
+        </Link>
+        <Link
+          to="/sos"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          SOS
+        </Link>{" "}
+        <Link
+          to="/posts"
+          className="nav-link"
+          onClick={() => setHamburgerOpen(false)}
+        >
+          Posts
+        </Link>
+      </ul>
+
+      <nav className="Nav-fs flex-item">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/onduty" className="nav-link">
+          OnDuty
+        </Link>
+        <Link to="/posts" className="nav-link">
+          Posts
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+        <Link to="/sos" className="nav-link">
+          SOS
+        </Link>
+        <Link className="nav-link" to="/login">
+          <FaUserAlt id="User-icon-fs" />
+        </Link>
+      </nav>
     </div>
   );
 }
